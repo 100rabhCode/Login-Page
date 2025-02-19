3a3376520a726f9f6c1e2c3414745e20034a4dc5
100rabhCode <saura1129bh@gmail.com>
2025-02-19 21:09:36 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,59 @@
+*{
+  margin: 0 px;
+  padding: 0 px;
+}
+body {
+  display: flex;
+  justify - content: center;
+  justify - items: center;
+  align - items: center;
+  background - image: url("/pink-blur-background-cc-1280x720.jpg");
+  background - repeat: no - repeat;
+  background - size: auto;
+
+
+
+
+}
+.container {
+  display: flex;
+  justify - content: center;
+  align - items: center;
+}
+label {
+  font - size: 30 px;
+}
+i {
+  color: rgba(255, 255, 255, .5);
+  font - size: 50 px;
+}
+form {
+  padding: 200 px 0 px;
+  text - align: center;
+}
+input {
+  height: 20 px;
+  width: 67 % ;
+  padding: 10 px 20 px;
+  border: none;
+  border - radius: 5 px;
+  background: rgba(255, 255, 255, 0.1);
+  margin - left: 2 px;
+  color: white;
+
+}
+form #pass {
+  margin - left: 10 px;
+}
+button {
+  padding: 12 px 30 px;
+  border: none;
+  border - radius: 5 px;
+  margin: 20 px 0 px;
+}
+a {
+  text - decoration: none;
+}
+h5 {
+  color: white;
+}
